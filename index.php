ffc30c82ef6e59220715b795fa254f1f342bd5f4
WallaceBarros <w.wallacebarros.cruz@gmail.com>
2024-07-21 19:48:37 -0300

    require e includ

--- index.php
+++ index.php
@@ -2,12 +2,18 @@
 
 <?php
 
-//comentario em linha 1
-
-#comentario em linha 2
-
 /*
-comentario
-em
-bloco
+
+includ: Adiciona um arquivo ao atual de forma independente, ou seja o arquivo funciona mesmo se o include não funcionar
+include 'config.php';
+
+require: Adiciona um arquivo ao atual de forma integrada, ou seja se o requere não funcionar o arquivo como um todo não funciona
+_once: Faz com que o arquivo seja caregado apenas uma unica vez
+
 */
+
+require_once 'system/config.php';
+echo '</br>';
+include 'helpers.php';
+
+?>
